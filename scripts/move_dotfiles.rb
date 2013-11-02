075e757e21dcdc08b26d935fa625eed16d086ec9
Max Jacobson <maxwell.jacobson@gmail.com>
2013-11-02 01:41:17 -0400

    i am dumb

--- scripts/move_dotfiles.rb
+++ scripts/move_dotfiles.rb
@@ -3,5 +3,6 @@
 path_to_dotfiles = File.expand_path("~/dotfiles")
 dotfiles = Dir.entries(path_to_dotfiles) - ['.', '..', 'scripts']
 dotfiles.each do |dotfile|
+  puts dotfile.inspect
   FileUtils.mv dotfile, "../#{dotfile}"
 end
