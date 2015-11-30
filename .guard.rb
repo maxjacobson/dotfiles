1c986942048ed0b0f6b00e8e3c503f0304cb69e4
Max Jacobson <max@hardscrabble.net>
2015-11-30 14:19:42 -0500

    Add a guard config file

---
+++ .guard.rb
@@ -0,0 +1,8 @@
+# alternately:
+# notification :off
+notification :tmux,
+             display_message: true,
+             default_message_color: 'black',
+             success: 'blue',
+             failure: 'red',
+             timeout: 1
