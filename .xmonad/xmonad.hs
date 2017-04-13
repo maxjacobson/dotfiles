a54d3e97370ddc97ca9bc6b3d6d6ec11030f0537
Max Jacobson <max@hardscrabble.net>
2017-04-13 00:39:06 -0400

    Customize xmonad style a bit
    thick hot pink borders, except when an app is in full screen
    
    (layouthook via wfleming's dotfiles)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,16 @@
 import XMonad
+import XMonad.Layout.NoBorders
+
+mylayoutHook = smartBorders tiled ||| smartBorders (Mirror tiled) ||| noBorders Full
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 3/100
 
 main = xmonad $ def
-   { borderWidth        = 1
-   , normalBorderColor  = "#333333"
-   , focusedBorderColor = "#999999"
+   { borderWidth        = 8
+   , normalBorderColor  = "#d6f7de"
+   , focusedBorderColor = "#f442b3"
+   , layoutHook = mylayoutHook
    , terminal           = "urxvt" }
