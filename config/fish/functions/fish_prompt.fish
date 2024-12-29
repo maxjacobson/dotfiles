4502599267da38ee58d3439009fc76a2be05dca4
Max Jacobson <max@hardscrabble.net>
2024-12-29 05:15:42 -0500

    customize fish git prompt a bit
    Enable colors, enable some more information, use some of the symbols
    used in pure

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,8 +1,19 @@
 function fish_prompt
   set -l last_status $status
-  set -g __fish_git_prompt_showupstream informative
+  set -g __fish_git_prompt_show_informative_status true
+  set -g __fish_git_prompt_showdirtystate true
+  set -g __fish_git_prompt_showuntrackedfiles true
+  set -g __fish_git_prompt_showstashstate true
+  set -g __fish_git_prompt_showcolorhints true
+  set -g __fish_git_prompt_char_stashstate "≡"
+  set -g __fish_git_prompt_char_upstream_diverged "↑↓"
+  set -g __fish_git_prompt_color_prefix "white"
+  set -g __fish_git_prompt_color_suffix "white"
+  set -g __fish_git_prompt_char_stateseparator " "
+  set -g __fish_git_prompt_char_cleanstate ""
 
-  string join '' -- (set_color blue) (prompt_pwd --full-length-dirs 2) (set_color 5D5D5D) (fish_vcs_prompt) (set_color normal)
+  echo ""
+  string join '' -- (set_color blue) (prompt_pwd --full-length-dirs 2) (fish_vcs_prompt) (set_color normal)
 
   if test "$last_status" -ne 0
     string join '' -- (set_color red) '❯ '
