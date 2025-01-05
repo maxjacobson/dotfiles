ee158973671c9d83e03d1ae4d5a129e260918056
Max Jacobson <max@hardscrabble.net>
2025-01-05 16:09:29 -0500

    Update fish prompt
    - use long flags and doc comments for future reference
    - remove some unnecessary configs
    - only include git info, not other vcses that i don't use

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,20 +1,28 @@
 function fish_prompt
-  set -l last_status $status
-  set -g __fish_git_prompt_show_informative_status true
-  set -g __fish_git_prompt_showdirtystate true
-  set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_showcolorhints true
-  set -g __fish_git_prompt_char_stashstate "≡"
-  set -g __fish_git_prompt_char_upstream_diverged "↑↓"
-  set -g __fish_git_prompt_color_prefix "white"
-  set -g __fish_git_prompt_color_suffix "white"
-  set -g __fish_git_prompt_char_stateseparator " "
-  set -g __fish_git_prompt_char_cleanstate ""
+  # Check if the last command succeeded so we can color the prompt red when it
+  # did not succeed
+  set --local last_status $status
 
+  # Configure fish_git_prompt to show some more info
+  set --global __fish_git_prompt_show_informative_status true
+  set --global __fish_git_prompt_showdirtystate true
+  set --global __fish_git_prompt_showuntrackedfiles true
+  set --global __fish_git_prompt_showstashstate true
+  set --global __fish_git_prompt_showcolorhints true
+
+  # Configure fish_git_prompt symbols
+  set --global __fish_git_prompt_char_stashstate "≡" # inspired by pure
+  set --global __fish_git_prompt_char_cleanstate "" # idc
+
+  # blank line before each prompt
   echo ""
-  string join '' -- (set_color blue) (prompt_pwd --full-length-dirs 2) (fish_vcs_prompt) (set_color normal)
 
+  # print some info
+  string join '' -- \
+    (set_color blue) (prompt_pwd --full-length-dirs 2) (set_color normal) \
+    (fish_git_prompt)
+
+  # print the actual prompt
   if test "$last_status" -ne 0
     string join '' -- (set_color red) '❯ '
   else
