94ac4c0c45e093476ff9cbf35dcb4018daf2342b
Max Jacobson <max@hardscrabble.net>
2025-01-19 19:14:53 -0500

    prefer fish abbreviations to git aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -71,6 +71,28 @@ if status is-interactive
     abbr --add to tmux attach-session -t
     abbr --add tl tmux list-sessions
 
+    #### Git command abbreviations
+
+    abbr --command git aa aa -A
+    abbr --command git authors shortlog --summary --numbered
+    abbr --command git co checkout
+    abbr --command git cp cherry-pick
+    abbr --command git cpa cherry-pick --abort
+    abbr --command git cpc cherry-pick --continue
+    abbr --command git dc diff --cached
+    abbr --command git l "log --pretty='%Cred%h %Creset%<|(58,trunc)%s%n%Cgreen%aN%Creset, %ar%n'"
+    abbr --command git pick add . -p
+    abbr --command git pro pull --rebase origin
+    abbr --command git pru pull --rebase upstream
+    abbr --command git ra rebase --abort
+    abbr --command git rc rebase --continue
+    abbr --command git recent-authors shortlog --summary --numbered --since='2 weeks ago'
+    abbr --command git rh reset HEAD --hard
+    abbr --command git rs rebase --skip
+    abbr --command git st status
+    abbr --command git sunrise commit --allow-empty --message '🌅'
+    abbr --command git unstage restore --staged .
+
     # auto-load my ssh key
     # and read the passphrase from the keychain so I don't need to enter it every time
     ssh-add -q --apple-use-keychain
