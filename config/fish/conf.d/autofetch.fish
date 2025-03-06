7b57d449b5943519396a74b8cddfefa9b11aa48b
Max Jacobson <max@hardscrabble.net>
2025-03-06 13:44:01 +0000

    update autofetch behavior
    - Make it happen in the background so that it doesn't block rendering
      the prompt (which is particularly annoying when offline, because it
      hangs for a while)
    - Make sure it works offline fine, and just quietly does nothing.
      - Perhaps has some risk of quietly failing entirely, which hopefully I
        will notice
    - Make sure it works on freshly-cloned repos

--- config/fish/conf.d/autofetch.fish
+++ config/fish/conf.d/autofetch.fish
@@ -1,10 +1,18 @@
 function autofetch --on-event fish_prompt --description 'Fetch on prompt'
-    if test -f ".git/FETCH_HEAD"
-        set --local mtime (path mtime --relative .git/FETCH_HEAD)
+    if test -d ".git"
+        set --local mtime
+
+        # FETCH_HEAD doesn't exist on a freshly-cloned repo
+        if test -f ".git/FETCH_HEAD"
+            set mtime (path mtime --relative .git/FETCH_HEAD)
+        else
+            set mtime (path mtime --relative .git/HEAD)
+        end
+
         set --local duration (math '10 * 60')
 
         if test "$mtime" -gt "$duration"
-            git fetch --prune --prune-tags --quiet
+            $HOME/bin/autofetch &
         end
     end
 end
