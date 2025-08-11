ffa86ccdf41c780f1a605b92fadd2bbb887b410d
Max Jacobson <max@hardscrabble.net>
2025-08-11 11:27:10 -0400

    Disable mini.pairs neovim plugin
    I feel like I'm not sure this is actually for me...

--- config/nvim/lua/plugins/mini.nvim.lua
+++ config/nvim/lua/plugins/mini.nvim.lua
@@ -1,7 +1,7 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    require('mini.pairs').setup()
+    -- require('mini.pairs').setup()
     require('mini.indentscope').setup()
     require('mini.trailspace').setup()
   end,
