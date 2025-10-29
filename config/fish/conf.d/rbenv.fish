666a3809e395243f9455b7e219e4c53cee755b70
Max Jacobson <max@hardscrabble.net>
2025-10-29 17:46:36 -0400

    Configure RBENV_ROOT
    In general trying to get things out of my home directory feels like a
    positive thing. This moves ~/.rbenv to ~/.cache/rbenv, which seems
    reasonable enough.

--- config/fish/conf.d/rbenv.fish
+++ config/fish/conf.d/rbenv.fish
@@ -1,3 +1,6 @@
+# Configure rbenv to store rubies here instead of the default of ~/.rbenv
+set --global --export RBENV_ROOT "$HOME/.cache/rbenv"
+
 # Enables rbenv for this shell session
 # install rbenv with `brew install rbenv`
 eval "$(rbenv init - fish)"
