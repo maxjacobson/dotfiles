b61a8d6cad69a492f158c927bb5b61993fcbd6d2
Max Jacobson <max@hardscrabble.net>
2026-01-07 09:26:25 -0500

    Streamline ripgrep config
    I think this seems fine to just have one config to use everywhere now...
    not sure since when but it seems to work fine (with clickable links in
    vscode terminal

--- config/fish/conf.d/ripgrep.fish
+++ config/fish/conf.d/ripgrep.fish
@@ -1,5 +1,1 @@
-if test "$TERM_PROGRAM" = vscode
-    set --global --export RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep-for-vscode"
-else
-    set --global --export RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep"
-end
+set --global --export RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
