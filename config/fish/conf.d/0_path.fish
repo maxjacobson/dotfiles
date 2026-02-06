21c119eda1c61dbac8829b47869dce61348800d3
Max Jacobson <max@hardscrabble.net>
2026-02-06 10:20:04 -0500

    Add ~/.local/bin to PATH

--- config/fish/conf.d/0_path.fish
+++ config/fish/conf.d/0_path.fish
@@ -11,3 +11,6 @@ end
 # This is a folder where I put custom helper scripts
 # Most of them are stored in my dotfiles git repo and synced there by rcm
 fish_add_path --path "$HOME/bin"
+
+# Claude Code puts its bin here
+fish_add_path --path "$HOME/.local/bin"
