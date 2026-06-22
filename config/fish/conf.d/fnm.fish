e329b1c309ba93c98d579987a09dc838e795c76e
Max Jacobson <max@hardscrabble.net>
2026-06-22 12:21:54 -0400

    Remove --corepack-enabled
    This doesn't work with node.js 25+, which doesn't bundle corepack with
    node

--- config/fish/conf.d/fnm.fish
+++ config/fish/conf.d/fnm.fish
@@ -1,4 +1,4 @@
 # Fast node manager
 # https://github.com/Schniz/fnm
 
-fnm env --use-on-cd --shell fish --log-level quiet --corepack-enabled | source
+fnm env --use-on-cd --shell fish --log-level quiet | source
