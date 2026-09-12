dd2d770e634cccf8a14f58cd82cf8c0ddbafd45c
Max Jacobson <max@hardscrabble.net>
2026-09-12 19:42:52 -0400

    commit linux version of fnm config

--- config/fish/conf.d/fnm.fish
+++ config/fish/conf.d/fnm.fish
@@ -1,4 +1,13 @@
 # Fast node manager
 # https://github.com/Schniz/fnm
 
-fnm env --use-on-cd --shell fish --log-level quiet | source
+# fnm
+set FNM_PATH "$HOME/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+    # Linux???
+    set PATH "$FNM_PATH" $PATH
+    fnm env --use-on-cd --log-level quiet --shell fish | source
+else
+    # macOS???
+    fnm env --use-on-cd --shell fish --log-level quiet | source
+end
