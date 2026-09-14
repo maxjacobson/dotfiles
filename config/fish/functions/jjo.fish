61c5326f1f8e8a715a5c73277e05a37f7928bcfc
Max Jacobson <max@hardscrabble.net>
2026-09-13 23:55:46 -0400

    Improve jjo function
    - fix a typo
    - when we can't identify the bookmark, just open the root of the repo

--- config/fish/functions/jjo.fish
+++ config/fish/functions/jjo.fish
@@ -18,11 +18,6 @@ function jjo --description "Open current branch on GitHub"
     set bookmarks (string split ";;;" "$bookmarks")
     set --local bookmark $bookmarks[1]
 
-    if test -z "$bookmark"
-        echo "Working copy @ has no bookmark"
-        return 1
-    end
-
     set --local repo
 
     set --local remotes (jj git remote list)
@@ -35,10 +30,14 @@ function jjo --description "Open current branch on GitHub"
         end
     end
 
-    if test -z "$bookmark"
+    if test -z "$repo"
         echo "Could not work out the remote"
         return 1
     end
 
-    gh browse --repo "$repo" --branch "$bookmark"
+    if test -n "$bookmark"
+        gh browse --repo "$repo" --branch "$bookmark"
+    else
+        gh browse --repo "$repo"
+    end
 end
