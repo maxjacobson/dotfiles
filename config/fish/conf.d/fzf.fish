635f4e235f9677701453c4817c955036c822c2ec
Max Jacobson <max@hardscrabble.net>
2026-09-18 13:29:51 -0400

    Update `FZF_DEFAULT_COMMAND` to work better with jj
    I noticed my fuzzy finding was occasionally slow in non-colocated jj
    repos. In those, the .git directory is not present, and so fd figures it
    doesn't need to consult `.gitignore` -- even though I want it to!
    
    Thankfully it seems to work just fine even when there is no `.gitignore`

--- config/fish/conf.d/fzf.fish
+++ config/fish/conf.d/fzf.fish
@@ -3,5 +3,5 @@ if status is-interactive
     #
     # This is configuration for the fuzzy file finder I use in vim
     # https://github.com/junegunn/fzf.vim
-    set --global --export FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --exclude .jj"
+    set --global --export FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --exclude .jj --ignore-file .gitignore"
 end
