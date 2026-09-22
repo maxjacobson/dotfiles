c9da36106c88e1a32281c03d23a9e7520288ae19
Maxwell Jacobson <max@hardscrabble.net>
2026-09-21 23:34:34 -0400

    Rename on-change-dir

--- config/fish/conf.d/on-change-dir.fish
+++ config/fish/conf.d/on-change-dir.fish
@@ -1,4 +1,4 @@
-function on_change_dir --on-variable PWD --description "Hooks to run on changing directory"
+function on-change-dir --on-variable PWD --description "Hooks to run on changing directory"
     if test -x .git/on-change-dir
         .git/on-change-dir
     else if test -x .jj/on-change-dir
