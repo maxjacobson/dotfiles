d7e6e1407392360e884d868d385fc0a6aa83e076
Maxwell Jacobson <max@hardscrabble.net>
2026-09-25 11:19:29 -0400

    Try telescope instead of fzf
    I've struggled to make fzf fast with jj

--- config/fish/conf.d/fzf.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # FZF
-    #
-    # This is configuration for the fuzzy file finder I use in vim
-    # https://github.com/junegunn/fzf.vim
-    set --global --export FZF_DEFAULT_COMMAND fuzzy-find-files-list
-end
---
+++ config/nvim/lua/plugins/telescope.nvim.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- optional but recommended
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  keys = {
+    { '<C-p>', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+  },
+}
